194a0672e79329c6be2747ed33c185bb1e54df43
ainazkambar <100521918+ainazkambar@users.noreply.github.com>
2023-01-22 15:58:27 +0600

    gulp
    Установка gulp и создание файла package.json

---
+++ gulp/config/path.js
@@ -0,0 +1,35 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath. basename (nodePath. resolve () );   //Получаем имя папки проекта
+const buildFolder = `./dist`;   //Папка с результатами
+const srcFolder = `./src`;   //Папка с исходниками
+
+export const path = {   //Создаем общий объект path
+    build: {
+        images: `${buildFolder}/img/`,
+        fonts: `${buildFolder}/fonts/`,
+        js: `${buildFolder}/js/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        files: `${buildFolder}/files/`
+    },
+    src: {
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        js: `${srcFolder}/js/app.js`,
+        scss: `${srcFolder}/scss/style.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`
+    },
+    watch: {
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        files: `${srcFolder}/files/**/*.*`
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: ``
+}
